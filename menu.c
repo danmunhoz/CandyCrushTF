914622c181fdf8669659602aea2704ad020aac5e
danmunhoz <danmunhoz@outlook.com>
2014-06-10 14:48:19 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-void jogo();
+struct jogadores
+{
+    char nome[20];
+    int p;
+};
 
 int menu()
 {
-   int opcao;
-   char nome[30];
 
-    printf("\n\t Menu: \n\n \tJogar(1) \n \tSair (2)\n \tPontuacao(3) \n\n Escolha: ");
+    struct jogadores jogador[6];
+    int opcao,records[6];
+
+    printf("\n\t Menu: \n\n \tJogar(1) \n \tSair (2)\n \tPontuacao(3)\n\tDicas(4) \n\n Escolha: ");
     scanf("%d",&opcao);
 
     switch(opcao)
     {
-        case 1: printf("Informe seu nome: ");
-                scanf("%s",nome);
-                jogo();break;
-        case 2: printf("\t\n Voce finalizou o jogo\n\n\tQue pena T-T\n\n");
+        case 1:
+                printf("Informe seu nome: ");
+                scanf("%s", jogador[6].nome);
+                jogo(jogador[6]);break;
+        case 2:
+                printf("\t\n Voce finalizou o jogo\n\n\tQue pena T-T\n\n");
                 return -1;
+        case 3:
+        case 4:
+                instrucoes();
+                break;
     }
+    return 0;
 }
