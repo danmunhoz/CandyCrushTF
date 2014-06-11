a89ee74195b3d12230c85af76db246d1dc2ed143
danmunhoz <danmunhoz@outlook.com>
2014-06-11 14:19:49 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/*  LABORATIRIO DE PROGRAMAÇÃO I - Trabalho Final
+    Daniel Munhoz
+    Gustavo Correa
+    Professor: Marco Mangan
+    T:490
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "menu.h"
+
+int main()
+{
+   menu(); //funcao menu.
+
+ return 0;
+}
