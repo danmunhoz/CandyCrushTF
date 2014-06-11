cd73d2c2b5ea2a6bdc6ba7c7ef2e842b07afbbe9
guscorrea <guscorrea@users.noreply.github.com>
2014-06-11 14:13:35 -0300

    Update mostrar.c

--- mostrar.c
+++ mostrar.c
@@ -5,18 +5,20 @@
 
 void mostrar(char m[SIZE][SIZE], int tam)
 {
+    // Cores:
     const char *const green = "\033[0;40;32m";
     const char *const yellow = "\033[0;40;33m";
     const char *const red = "\033[0;40;31m";
     const char *const blue = "\033[0;40;34m";
     const char *const normal = "\033[0m";
+    const char *const purple = "\033[0;40;35m";
 
-    int i, j,x;
-    printf("     0 1 2 3 4 5 6 7 8\n\n");
+    int i,j;
+    printf("    0 1 2 3 4 5 6 7 8\n\n"); // Colunas
     for(i = 0; i < tam; i++)
     {
-        printf("%s%d   %s",normal,i,normal);
-        for(j = 0; j < tam; j++)
+        printf("%s%d  %s",normal,i,normal); // Linhas
+        for(j = 0; j < tam; j++) // Mostra os doces
         {
             if (m[i][j] == '.')
             printf("%s %c%s",normal, m[i][j],normal);
@@ -28,10 +30,12 @@ void mostrar(char m[SIZE][SIZE], int tam)
             printf("%s %c%s", blue, m[i][j],blue);
             if (m[i][j] == 'R')
             printf("%s %c%s",red, m[i][j],red);
+            if (m[i][j] == 'P')
+            printf("%s %c%s",purple, m[i][j],purple);
 
         }
-        printf("%s   %d%s",normal,i,normal);
+        printf("%s  %d%s",normal,i,normal); // Linhas
         printf("\n");
     }
-    printf("%s%s",normal);
+    printf("%s",normal);
 }
