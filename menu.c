caeca4e9afe4858952cdfd52b073dcc66d73f78a
guscorrea <guscorrea@users.noreply.github.com>
2014-06-11 14:14:21 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "jogo.h"
+#include "instrucoes.h"
+#include "jogador.h"
+#include "records.h"
 
-struct jogadores
-{
-    char nome[20];
-    int p;
-};
+struct jogador jogadores[6];
 
 int menu()
 {
 
-    struct jogadores jogador[6];
-    int opcao,records[6];
+    records(); //leitura do arquivo onde estao as pontuacoes.
 
+    int opcao,i;
     printf("********************************************************************************\n");
     printf("\t **      *     *  *  **    *   *    **  ***   *   *   * *   *   *  \n ");
     printf("\t*       * *    ** *  *  *    *     *    *  *  *   *   *     * * *  \n");
@@ -23,19 +23,30 @@ int menu()
     printf("\n\t\t\t Menu:\t \n\n \t\t\tJogar(1)\t \n \t\t\tSair(2)\t\n \t\t\tPontuacao(3)\t\n\t\t\tDicas(4)\t \n\n \t\t\tEscolha: ");
     scanf("%d",&opcao);
 
-    switch(opcao)
+    switch(opcao) //opcoes do menu.
     {
-        case 1:
-                printf("Informe seu nome: ");
-                scanf("%s", jogador[6].nome);
-                jogo(jogador[6]);break;
-        case 2:
-                printf("\t\n Voce finalizou o jogo\n\n\tQue pena T-T\n\n");
-                return -1;
-        case 3:
-        case 4:
-                instrucoes();
-                break;
+    case 1: //começa o jogo.
+        printf("Informe seu nome: ");
+        scanf("%s", jogadores[5].nome);
+        jogo(jogadores);
+        break;
+    case 2: //finaliza o jogo.
+        printf("\t\n Voce finalizou o jogo\n\n\tQue pena T-T\n\n");
+        return -1;
+    case 3: //mostra as pontuaçoes.
+        i = 0;
+        printf("\n\n\t\tMelhores pontuacoes:\n\n");
+        while (i < 5)
+        {
+            printf("\t\t%s, %d\n", jogadores[i].nome, jogadores[i].p);
+            i++;
+        }
+        printf("\n\n");
+        menu();
+        break;
+    case 4: //Mostra as instruçoes.
+        instrucoes();
+        break;
     }
     return 0;
 }
